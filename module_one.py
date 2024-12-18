a9c80c856626875670095993b4358e3f701271a8
emiliamaurer <em080@hdm-stuttgart.de>
2024-12-18 12:27:38 +0100

    Überarbeitung Customgpt

--- module_one.py
+++ module_one.py
@@ -2,9 +2,23 @@
 import streamlit as st
 from gpt_integration import generate_content
 
+
+
 def app():
     st.title("Content-Generierung")
     st.subheader("Erstelle kreative Marketing-Inhalte")
+    st.markdown("""
+    *Ziel:* Erstellung und Bearbeitung von Marketing-Inhalten.
+
+    *Funktionen:*
+    - *Post-Ideen-Generator*: Liefert kreative und relevante Vorschläge für Beiträge, die auf Zielgruppe und Marketingzielen basieren.
+    - *Inhalte generieren*: Erzeugt Texte auf Basis von Angaben wie Produktinformationen, Zielgruppe und Marketingzielen.
+    - *Anpassungsmöglichkeiten & Tone Adjuster*: Ermöglicht die direkte Bearbeitung der generierten Inhalte und bietet Optionen, den Ton anzupassen (formal, humorvoll, informativ etc.).
+    - *Caption Creator*: Erstellt ansprechende Bildunterschriften, die individuell angepasst werden können.
+    - *Automatische Hashtag-Generierung*: Generiert relevante und zielgerichtete Hashtags zur Optimierung der Beiträge.
+    """)
+
+    st.markdown("---")
 
     # Benutzerdefinierten Prompt erstellen
     company_profile = st.session_state.get("company_profile", None)
